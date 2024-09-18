3e7b779350fe9b7adba37721900714a07b7f5383
YohanGh <YohanGH@proton.me>
2024-09-18 12:01:27 +0200

    Add components benefit and fix page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,15 +3,16 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="min-h-[99vh] px-2 py-8 flex flex-col gap-3 items-start">
-      <div>
-        <h2 className="text-5xl font-bold">404</h2>
-        <p className="text-muted-foreground">Page not found</p>
+    <div className="flex min-h-[88vh] flex-col items-center justify-center px-2 py-8 text-center sm:min-h-[91vh]">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h2 className="mb-4 text-3xl font-bold sm:text-7xl">Page not found</h2>
       </div>
-
-      <Link href="/" className={buttonVariants({})}>
-        Back to homepage
-      </Link>
+        <p className="mb-4 text-3xl font-bold sm:text-5xl">404</p>
+      <div className="flex flex-row items-center gap-5">
+        <Link href="/" className={buttonVariants({})} >
+          Back to homepage
+        </Link>
+      </div>
     </div>
   );
 }
