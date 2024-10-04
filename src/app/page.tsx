bde30c4fe1ee40980637902d5fb93a528b8d2f71
YohanGH <YohanGH@proton.me>
2024-10-04 22:00:24 +0200

    Update homepage according t ocurrents needs deletion of sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import { BenefitsSection } from "@/components/sections/benefits";
-import { CommunitySection } from "@/components/sections/community";
-import { ContactSection } from "@/components/sections/contact";
-import { FAQSection } from "@/components/sections/faq";
 import { FeaturesSection } from "@/components/sections/features";
-import { FeedbackSection } from "@/components/sections/feedback";
 import { HeroSection } from "@/components/sections/hero";
-import { PricingSection } from "@/components/sections/pricing";
 import { ServicesSection } from "@/components/sections/services";
 import { SponsorsSection } from "@/components/sections/sponsors";
 import { TeamSection } from "@/components/sections/team";
@@ -18,12 +13,7 @@ export default function Home() {
       <BenefitsSection />
       <FeaturesSection />
       <ServicesSection />
-      <FeedbackSection />
       <TeamSection />
-      <CommunitySection />
-      <PricingSection />
-      <ContactSection />
-      <FAQSection />
     </>
   );
 }
